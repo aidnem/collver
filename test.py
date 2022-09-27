90d7531b322a8f1c0ae2aeab0f2f6aa0f36ff752
aidnem <99768676+aidnem@users.noreply.github.com>
2022-09-27 17:02:44 -0400

    Fix `test.py` script to actually provide tests with correct `stdin`

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class TestSpec:
     spec_path: str
     file_path: str
     compiler_stderr: bytes
-    provided_input: str
+    provided_input: bytes
     expected_stdout: bytes
     expected_stderr: bytes
 
@@ -45,7 +45,7 @@ def eat_chunk(rlines: list[str]) -> str:
 
 def parse_spec(spec_path: str) -> TestSpec:
     """Parse a test spec file into a text spec object"""
-    spec = TestSpec(spec_path, "", b"", "", b"", b"")
+    spec = TestSpec(spec_path, "", b"", b"", b"", b"")
     with open(spec_path, "r") as f:
         lines = f.readlines()
 
@@ -58,7 +58,8 @@ def parse_spec(spec_path: str) -> TestSpec:
         if line == "[!COMERR]\n":
             spec.compiler_stderr = eat_chunk(rlines).encode("utf-8")
         elif line == "[!STDIN]\n":
-            spec.provided_input = eat_chunk(rlines)
+            chunk = eat_chunk(rlines)
+            spec.provided_input = chunk.encode("utf-8")
         elif line == "[!STDOUT]\n":
             spec.expected_stdout = eat_chunk(rlines).encode("utf-8")
         elif line == "[!STDERR]\n":
@@ -66,8 +67,11 @@ def parse_spec(spec_path: str) -> TestSpec:
 
     return spec
 
-def run_echoed(cmd: list[str], print_outs: bool=False):
+def print_cmd(cmd: list[str]):
     print(f"<CMD> {' '.join(cmd)}")
+
+def run_echoed(cmd: list[str], print_outs: bool=False):
+    print_cmd(cmd)
     run_res = subprocess.run(cmd, capture_output=True)
     if print_outs:
         out = run_res.stdout.decode("utf-8")
@@ -78,14 +82,15 @@ def run_echoed(cmd: list[str], print_outs: bool=False):
     return run_res
 
 def run_spec(spec: TestSpec) -> TestResult:
-    com_res = run_echoed(["python3.10", "collver.py", "to-ll", spec.file_path])
-    if com_res.returncode != 0:
-        return TestResult(spec, com_res.stderr, False, b"", b"")
+    res = run_echoed(["python3.10", "collver.py", "to-ll", spec.file_path])
+    if res.returncode != 0:
+        return TestResult(spec, res.stdout, False, b"", b"")
     ll_path = os.path.splitext(spec.file_path)[0] + ".ll"
     run_echoed(["python3.10", "collver.py", "from-ll", ll_path])
     bin_path = os.path.splitext(spec.file_path)[0]
-    run_res = run_echoed([bin_path])
-    return TestResult(spec, com_res.stderr, True, run_res.stdout, run_res.stderr)
+    print_cmd([bin_path])
+    run_res = subprocess.run([bin_path], input=spec.provided_input, capture_output=True)
+    return TestResult(spec, res.stderr, True, run_res.stdout, run_res.stderr)
 
 @dataclass
 class Problem:
